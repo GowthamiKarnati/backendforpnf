b0fb3815227328406b7c9f07e96f111e3100ec35
Gowthami karnati <karnatigowthami43@gmail.com>
2024-02-07 16:35:56 +0530

    iygduvyrw

--- index.js
+++ index.js
@@ -448,7 +448,7 @@ app.post("/create", async (req, res) => {
     const sheetId = 59283844;
 
     
-    const { numberOfTires, selectedBrand, loanAmount, name, pan, mobilenumber, alternatemobile, martialstatus, numofchildren, housetype, trucknumber, date,numberoftrucks,source, insurancetype, monthlyemioutflow,numberOfYearsInBusiness } = req.body;
+    const { numberOfTires, selectedBrand, loanAmount, name, pan, mobilenumber, alternatemobile, martialstatus, numofchildren, housetype, trucknumber, date,numberoftrucks,source, insurancetype, monthlyemioutflow,numberOfYearsInBusiness,Totaldriversalary } = req.body;
     const sourceValue = source ? source : 'null';
     const sourceJsonValue = JSON.stringify({
       "reference_column_id": 236,
@@ -474,6 +474,7 @@ app.post("/create", async (req, res) => {
       "1208":{"value":insurancetype},
       "798": {"value": monthlyemioutflow},
       "796": {"value": numberOfYearsInBusiness}
+      "804":{"value": Totaldriversalary}
     });
     
     
